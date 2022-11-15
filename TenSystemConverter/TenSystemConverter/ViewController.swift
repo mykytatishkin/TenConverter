61ee98d97aa9c13b54ae37c136d114cc6132e5e8
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2022-11-15 11:50:14 +0200

    func raduxConverter()

--- TenSystemConverter/TenSystemConverter/ViewController.swift
+++ TenSystemConverter/TenSystemConverter/ViewController.swift
@@ -6,15 +6,30 @@
 //
 
 import UIKit
+import UserNotifications
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    let notificationCenter = UNUserNotificationCenter.current()
+    let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         _ = UITextField()
         eNum.keyboardType = .numberPad
         
+        
+        notificationCenter.requestAuthorization(options: options) {
+            (didAllow, error) in
+            if !didAllow {
+                print("User has declined notifications")
+
+            }
+        }
     }
     
     @IBAction func moreRadixButton(_ sender: Any) {
@@ -50,6 +65,32 @@ class ViewController: UIViewController {
         print("Button tapped")
         UIPasteboard.general.string = titleOfResult.text
     }
-
+    
+    func radixConverter(number: String, radix: Int) {
+        do {
+            if (eNum.text! == "") {
+           
+            }
+            
+            var r: Double? = Double(radix)
+            var I: Double? = Double(eNum.text!)
+            var reverseWriting: String = ""
+            var alphabet: [String] = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+            
+            let alphRange = alphabet.startIndex..<alphabet.endIndex
+            while(I! > Double(0)) {
+                reverseWriting += alphabet[Int(floor((I!).truncatingRemainder(dividingBy: r!)))]
+                I = floor(I! / r!)
+            }
+            for j in 0...reverseWriting.count {
+                var nIndex: Int = reverseWriting.count - j - 1
+                
+                titleOfResult.text! += (reverseWriting[nIndex])
+            }
+            
+        } catch {
+            print("Application work uncorrectly")
+        }
+    }
 }
 
